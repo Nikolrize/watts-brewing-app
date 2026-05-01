38114267521a5a10c4657b511622eb36fc6552dd
Nikolrize <nicolechen119@gmail.com>
2026-05-02 02:12:22 +0800

    Add: page.tsx station ranking api; Remove: stations from getDashboard api and type

--- watts-brewing-frontend/lib/api.ts
+++ watts-brewing-frontend/lib/api.ts
@@ -2,3 +2,8 @@ export async function getDashboard() {
   const res = await fetch("http://localhost:5000/api/dashboard");
   return res.json();
 }
+
+export async function getStationRanking() {
+  const res = await fetch("http://localhost:5000/api/stations");
+  return res.json();
+}
