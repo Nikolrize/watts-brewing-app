b25d0669f5f93982033e595b55e1810d505e796d
Nikolrize <nicolechen119@gmail.com>
2026-05-02 15:45:48 +0800

    Add: gemini ai api to generate insights

---
+++ watts-brewing-backend/src/routes/aiRoutes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+const { getAIInsights } = require("../controllers/aiController");
+
+router.get("/", getAIInsights);
+
+module.exports = router;
