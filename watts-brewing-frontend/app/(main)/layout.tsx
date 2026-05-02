c2f50aebd1ee0580d1283362c834363346e23d93
Nikolrize <nicolechen119@gmail.com>
2026-05-03 01:53:38 +0800

    Fix: dashboard keep redirecting to login; Add: dashboard-guard to check auth;

--- watts-brewing-frontend/app/(main)/layout.tsx
+++ watts-brewing-frontend/app/(main)/layout.tsx
@@ -1,27 +1,22 @@
 import { SidebarProvider } from "@/components/ui/sidebar";
 import CustomSidebar from "@/components/custom/custom-sidebar";
 import CustomHeaderWrapper from "@/components/custom/custom-header-wrapper";
-import { redirect } from "next/navigation";
-import { cookies } from "next/headers";
+import DashboardGuard from "@/components/custom/dashboard-guard";
 
 export default async function MainLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const token = (await cookies()).get("token")?.value;
-
-  if (!token) {
-    redirect("/login");
-  }
-
   return (
-    <SidebarProvider>
-      <CustomSidebar />
-      <div className="flex flex-col h-screen w-full overflow-hidden">
-        <CustomHeaderWrapper />
-        <main className="flex-1 overflow-y-auto">{children}</main>
-      </div>
-    </SidebarProvider>
+    <DashboardGuard>
+      <SidebarProvider>
+        <CustomSidebar />
+        <div className="flex flex-col h-screen w-full overflow-hidden">
+          <CustomHeaderWrapper />
+          <main className="flex-1 overflow-y-auto">{children}</main>
+        </div>
+      </SidebarProvider>
+    </DashboardGuard>
   );
 }
---
+++ watts-brewing-frontend/components/custom/dashboard-guard.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { ReactNode, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { getUser } from "@/lib/api";
+
+export default function DashboardGuard({ children }: { children: ReactNode }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const res = await getUser();
+
+        if (res?.success) {
+          router.replace("/dashboard");
+        } else {
+          router.replace("/login");
+        }
+      } catch (err) {
+        router.replace("/login");
+      }
+    };
+
+    checkAuth();
+  }, []);
+
+  return children;
+}
