bb493c7900918880e438d735e559c2d3ef4f241f
Nikolrize <nicolechen119@gmail.com>
2026-05-03 00:56:34 +0800

    Update: api.ts local url to deployed backend url

--- watts-brewing-frontend/lib/api.ts
+++ watts-brewing-frontend/lib/api.ts
@@ -1,22 +1,24 @@
 import { loginCredential } from "./types";
 
+const BASE_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
+
 export async function getDashboard() {
-  const res = await fetch("http://localhost:5000/api/dashboard");
+  const res = await fetch(`${BASE_URL}/api/dashboard`);
   return res.json();
 }
 
 export async function getStationRanking() {
-  const res = await fetch("http://localhost:5000/api/stations");
+  const res = await fetch(`${BASE_URL}/api/stations`);
   return res.json();
 }
 
 export async function getAIInsights() {
-  const res = await fetch("http://localhost:5000/api/ai");
+  const res = await fetch(`${BASE_URL}/api/ai`);
   return res.json();
 }
 
 export async function login(form: loginCredential) {
-  const res = await fetch("http://localhost:5000/api/auth/login", {
+  const res = await fetch(`${BASE_URL}/api/auth/login`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -29,7 +31,7 @@ export async function login(form: loginCredential) {
 }
 
 export async function getUser() {
-  const res = await fetch(`http://localhost:5000/api/user/me`, {
+  const res = await fetch(`${BASE_URL}/api/user/me`, {
     method: "GET",
     credentials: "include",
   });
