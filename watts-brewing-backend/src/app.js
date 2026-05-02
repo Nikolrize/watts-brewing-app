1aae3ada81bc4086f5890643dad4888646c22d5c
Nikolrize <nicolechen119@gmail.com>
2026-05-02 21:47:45 +0800

    Add: mongoDB connection and auth api

--- watts-brewing-backend/src/app.js
+++ watts-brewing-backend/src/app.js
@@ -5,6 +5,7 @@ const app = express();
 const dashboardRoutes = require("./routes/dashboardRoutes");
 const stationRoutes = require("./routes/stationRoutes");
 const aiRoutes = require("./routes/aiRoutes");
+const authRoutes = require("./routes/authRoutes");
 
 app.use(
   cors({
@@ -17,5 +18,6 @@ app.use(express.json());
 app.use("/api/dashboard", dashboardRoutes);
 app.use("/api/stations", stationRoutes);
 app.use("/api/ai", aiRoutes);
+app.use("/api/auth", authRoutes);
 
 module.exports = app;
